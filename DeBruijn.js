18e5d114e7f8163a4ad96e4442792d138662d199
Luoh Ren-Shan (LCamel) <lcamel@yahoo-inc.com>
2015-11-14 08:55:49 +0800

    add the broken IO version of DeBruijn.js

--- DeBruijn.js
+++ DeBruijn.js
@@ -1,23 +1,73 @@
 "use strict";
 
+function p(x) {
+    console.log("###" + x + "###");
+    return p;
+}
 function getInternal(name) {
-    //console.log("getInternal: name: " + "##" + name + "##");
+    console.log("getInternal: name: " + "##" + name + "##");
     if (name === "+") {
         return ["int", (x) => ["int", (y) => ["int", 
             deBruijnLeftmost(x, false)[1] + 
             deBruijnLeftmost(y, false)[1]
         ]]];
     }
+    if (name === "bind") {
+console.log("bind~~~~~~~~~~~~~~~~");
+        return ["int", function (ioa) {
+console.log("got ioa~~~~~~");
+            return ["int", function(f) {
+console.log("got f~~~~~~");
+                return ["int", function(w0) {
+                    var _w0 = deBruijnLeftmost(w0, false)[1];
+                    var _f = deBruijnLeftmost(f, false);
+console.log("00000");
+console.log(JSON.stringify(_f));
+console.log("1111");
+          _f = _f[1];
+                    var _ioa = deBruijnLeftmost(ioa, false)[1];
+console.log(">>>>>>");
+console.log("_w0:");
+console.log(_w0);
+console.log("_f:");
+console.log(_f.toString());
+console.log("_ioa.toString()");
+console.log(_ioa.toString());
+console.log("<<<<<<<");
+                    
+                    var [a, w1] = _ioa(_w0);
+                    return ["int", _f(a)(w1)];
+                }];
+            }];
+        }];
+    }
+            
     if (name === "print") {
         return ["int", (a) => ["int", (world) => {
             console.log("print: " + deBruijnLeftmost(a, false)[1]); 
-            return ["int", [undefined, deBruijnLeftmost(world, false)[1]]]
+            console.log("world: ");
+            console.log(world);
+            var io = [88, deBruijnLeftmost(world, false)[1]];
+            var tmp = ["int", io];
+            console.log("print returns: ")
+            console.log(tmp);
+            return tmp;
         }]];
     }
     if (name === "arrGet") {
-        return ["int", (arr) => ["int", (idx) => ["int", 
-            (deBruijnLeftmost(arr, false)[1]) [  deBruijnLeftmost(idx, false)[1]  ]
-        ]]];
+        return ["int", (arr) => ["int", (idx) => {
+            console.log("11111111");
+            var theArr = deBruijnLeftmost(arr, false)[1];
+            console.log("type of theArr: " + typeof(theArr));
+            console.log("value of theArr: " + theArr);
+            var theIdx = deBruijnLeftmost(idx, false)[1];
+            console.log("type of theIdx: " + typeof(theIdx));
+            console.log("value of theIdx: " + theIdx);
+            var tmp = ["int", theArr[theIdx]];
+            console.log("arrGet returns:");
+            console.log(tmp);
+            return tmp;
+        }]];
     }
 
 
@@ -78,6 +128,9 @@ function doA(a, B, aLamDepth) {
     if (a[0] === "lam") {
         return ["lam", doA(a[1], B, aLamDepth + 1)];
     }
+    if (a[0] === "int") {
+        return a;
+    }
     // var is just a number
     if (a === aLamDepth) {
         return doB(B, aLamDepth, 0);
@@ -93,6 +146,9 @@ function doB(b, aLamDepth, bLamDepth) {
     }
     if (b[0] === "lam") {
         return ["lam", doB(b[1], aLamDepth, bLamDepth + 1)];
+    }
+    if (b[0] === "int") {
+        return b;
     }
     // var is just a number
     if (b > bLamDepth) {
@@ -182,15 +238,17 @@ console.log(JSON.stringify(exp2));
 //var bind = "(λioa (λf (λw0 (  (λa_w1  ((f  ((arrGet a_w1) 0))  ((arrGet a_w1) 1))    )    (ioa w0)     ))))"
 //var exp = parse(`((λbind ( print 3 )) ${bind}   )`);
 var exp = parseFile("DeBruijn.ulc");
-console.log(JSON.stringify(exp));
+console.log("exp: " + JSON.stringify(exp));
 var db = toDeBruijn(exp, []);
-console.log(JSON.stringify(db));
+console.log("de Bruijn: "); // + JSON.stringify(db));
+console.log(db);
 console.log("=========");
 var db = deBruijnLeftmost(db, false);
 
 var exp2 = fromDeBruijn(db, [], [0]);
 //console.log(JSON.stringify(exp2));
-exp2[1]("world");
+console.log("======= going to apply world");
+exp2[1](["int", 999999]);
 /*
 function timeDiff(f) {
     const t0 = Date.now();
