3f1aa380847896a82a831427b0e50cce77ed2886
Luoh Ren-Shan (LCamel) <lcamel@yahoo-inc.com>
2015-11-14 08:53:42 +0800

    make run.sh detect all $* changes

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 S=$1
 touch $S
-while true; do [ $S -nt run.time ] && touch run.time && clear && node --harmony --harmony_destructuring $S; sleep 1; done
+    perl -e 'print "\n" x 10'
+while true; do
+    NEWEST=`ls -tr $* | tail -n 1`
+    [ $NEWEST -nt run.time ] && touch run.time && clear && perl -e 'print "\n" x 10' && node --harmony --harmony_destructuring $S;
+    sleep 1;
+done
