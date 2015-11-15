9b17fbfb7c3b15bb0cea96774751a5f2c9e0c6d9
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2015-11-15 16:50:51 +0800

    rename Parser.parse to jsonToExp

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,14 +2,14 @@ import org.json.JSONArray;
 
 
 public class Parser {
-    public static Exp parse(JSONArray a) {
+    public static Exp jsonToExp(JSONArray a) {
         switch (a.getString(0)) {
         case "var":
             return new Var(a.getString(1));
         case "app":
-            return new App(parse(a.getJSONArray(1)), parse(a.getJSONArray(2)));
+            return new App(jsonToExp(a.getJSONArray(1)), jsonToExp(a.getJSONArray(2)));
         default:
-            return new Lam(a.getString(1), parse(a.getJSONArray(2)));
+            return new Lam(a.getString(1), jsonToExp(a.getJSONArray(2)));
         }
     }
 }
