f3b5f8593d287abac2d0a45f4d00e35846dd5283
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2016-03-06 21:22:35 +0800

    comment

--- src/main/java/l2/AppReplacer.java
+++ src/main/java/l2/AppReplacer.java
@@ -3,6 +3,9 @@ package l2;
 
 // TODO: 這邊假設了 mutable 的作法, 要求有點多
 // 不過先這樣
+// TODO: 會有人光用 finder 而不用 replacer 嗎?
+// 分成兩個 interface 是讓 code 變好懂還是變難懂呢?
+//
 public interface AppReplacer extends AppFinder {
     public interface App extends AppFinder.App {
         void setLeft(Node node);  // mutable
