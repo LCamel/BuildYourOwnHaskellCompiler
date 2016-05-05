84c6cd22d1e1b898236a3ae5741524bda0a65a22
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2016-05-05 16:55:10 +0000

    parseModule

--- haskell/parse/app/Main.hs
+++ haskell/parse/app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import Lib
+import Language.Haskell.Exts.Annotated.Parser
+
 
 main :: IO ()
 -- main = someFunc
 main = do
          lines <- readFile "app/Main.hs"
-         putStrLn lines
+--         putStrLn lines
+         putStrLn $ show $ parseModule lines
