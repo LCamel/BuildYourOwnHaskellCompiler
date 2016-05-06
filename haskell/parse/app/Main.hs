d1fc7752d07398a9393b51afb290ee6426a7d855
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2016-05-06 15:00:05 +0000

    get import module name

--- haskell/parse/app/Main.hs
+++ haskell/parse/app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Lib
 import Language.Haskell.Exts.Annotated.Parser (parseModule)
 import Language.Haskell.Exts.Parser ( ParseResult(ParseOk, ParseFailed) )
-import Language.Haskell.Exts.Annotated.Syntax ( Module(Module) )
+import Language.Haskell.Exts.Annotated.Syntax ( Module(Module), ModuleName(ModuleName), ImportDecl(importModule) )
 import Language.Haskell.Exts.SrcLoc (SrcSpanInfo)
 
 
@@ -16,15 +16,21 @@ main = do
 
          case parseModule lines of
 --           ParseOk moduleWithSrcSpanInfo -> putStrLn "Ok"
-           ParseOk moduleWithSrcSpanInfo -> getImportsFromModule moduleWithSrcSpanInfo
+--           ParseOk moduleWithSrcSpanInfo -> getImportsFromModule moduleWithSrcSpanInfo
+           ParseOk moduleWithSrcSpanInfo -> putStrLn $ show $ getImportsFromModule moduleWithSrcSpanInfo
            ParseFailed _ _ -> putStrLn "Failed"
 
 -- parseModule :: String -> ParseResult (Module SrcSpanInfo)
 -- data ParseResult a
 --   ParseOk a
--- getImportsFromModule :: Module SrcSpanInfo -> [String]
-getImportsFromModule :: Module SrcSpanInfo -> IO ()
-getImportsFromModule (Module _ _ _ importDecl _) = putStrLn $ show importDecl
+getImportsFromModule :: Module SrcSpanInfo -> [String]
+--getImportsFromModule :: Module SrcSpanInfo -> IO ()
+getImportsFromModule (Module _ _ _ importDecls _) = map (getModuleName . importModule) importDecls
+
+getModuleName :: ModuleName l -> String
+getModuleName (ModuleName l name) = name
+
+
 
 {--
 https://hackage.haskell.org/package/haskell-src-exts-1.17.1/docs/Language-Haskell-Exts-Annotated-Parser.html
