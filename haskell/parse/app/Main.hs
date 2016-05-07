45b0028bf6769fab0ef8cfe39c75a1f4b7286fe4
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2016-05-07 15:23:54 +0000

    collector

--- haskell/parse/app/Main.hs
+++ haskell/parse/app/Main.hs
@@ -5,6 +5,9 @@ import Language.Haskell.Exts.Annotated.Parser (parseModule)
 import Language.Haskell.Exts.Parser ( ParseResult(ParseOk, ParseFailed) )
 import Language.Haskell.Exts.Annotated.Syntax ( Module(Module), ModuleName(ModuleName), ImportDecl(importModule) )
 import Language.Haskell.Exts.SrcLoc (SrcSpanInfo)
+-- import Data.Map (Map, empty)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 
 main :: IO ()
@@ -20,15 +23,35 @@ main = do
            ParseOk moduleWithSrcSpanInfo -> putStrLn $ show $ getImportsFromModule moduleWithSrcSpanInfo
            ParseFailed _ _ -> putStrLn "Failed"
 
+type MyModuleName = String
+
+parseModuleRecursive :: MyModuleName -> Map MyModuleName (Module foo) -> Map MyModuleName (Module foo)
+parseModuleRecursive = undefined
+
+
 -- parseModule :: String -> ParseResult (Module SrcSpanInfo)
 -- data ParseResult a
 --   ParseOk a
-getImportsFromModule :: Module SrcSpanInfo -> [String]
+getImportsFromModule :: Module foo -> [String]
 --getImportsFromModule :: Module SrcSpanInfo -> IO ()
 getImportsFromModule (Module _ _ _ importDecls _) = map (getModuleName . importModule) importDecls
 
-getModuleName :: ModuleName l -> String
-getModuleName (ModuleName l name) = name
+getModuleName :: ModuleName foo -> String
+getModuleName (ModuleName foo name) = name
+
+
+collector :: (Ord a) => a -> (a -> (b, [a])) -> Map a b
+collector start f = collector0 [start] f Map.empty
+
+collector0 :: (Ord a) => [a] -> (a -> (b, [a])) -> Map a b -> Map a b
+collector0 []     f m = m
+collector0 (a:as) f m = if Map.member a m
+                          then collector0 as f m
+                          else let
+                                 (b, as2) = f a
+                               in
+                                 collector0 (as ++ as2) f (Map.insert a b m)
+
 
 
 
